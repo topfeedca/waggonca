41e180605590209ab6e27f22929714ad13c7c9f3
Kyle <kyle@eastcode.ca>
2020-10-20 13:46:40 -0300

    update codebase

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
 
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
 	env: {
 		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
 		REPO_FULL_NAME: process.env.REPO_FULL_NAME,
 		BASE_BRANCH: process.env.BASE_BRANCH,
 	},
-	// ...
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+    }
+  },
+  assetPrefix: !debug ? 'https://anotherplanet-io.github.io/Next-React-Components/' : '',
 }
