9df332b84c34bca7813f3b15cc4201cb7d900207
Kyle Hensman <kyle@eastcode.ca>
2020-11-10 15:00:35 -0400

    fixed issues

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,140 +1,3 @@
-// import React from 'react'
-// import styled from 'styled-components'
-
-// export default function Footer(props) {
-
-//   return (
-//     <FooterWrapper>
-//       <FooterContent>
-//         <FooterColLg>
-//           <h3>Waggon</h3>
-//           <p>
-//             A robust CSA and farm share management tool that offers a unique way to track and interact with your customers.
-//           </p>
-//           <p className="copyright">
-//             © 2019 waggon All rights reserved.
-//           </p>
-//         </FooterColLg>
-//         <FooterCol>
-//           <ul>
-//             <li>
-//               <a href="/">Home</a>
-//             </li>
-//             <li>
-//               <a href="/">Product</a>
-//             </li>
-//             <li>
-//               <a href="/">Discover</a>
-//             </li>
-//             <li>
-//               <a href="/">Platform</a>
-//             </li>
-//           </ul>
-//         </FooterCol>
-//         <FooterCol>
-//           <ul>
-//             <li>
-//               <a href="/">Terms</a>
-//             </li>
-//             <li>
-//               <a href="/">Privacy</a>
-//             </li>
-//             <li>
-//               <a href="/">Cookies</a>
-//             </li>
-//             <li>
-//               <a href="/">Copyright</a>
-//             </li>
-//           </ul>
-//         </FooterCol>
-//         <FooterCol>
-//           <ul>
-//             <li>
-//               <a href="/support">24/7 Chat Support</a>
-//             </li>
-//             <li>
-//               <a href="/">info@waggon.ca</a>
-//             </li>
-//             <li>
-//               <a href="/">+1-800-555-1234</a>
-//             </li>
-//           </ul>  
-//         </FooterCol>
-//       </FooterContent>
-//     </FooterWrapper>
-//   );
-// }
-
-// const FooterWrapper = styled.footer`
-//   width: 100%;
-//   border-top: solid 1px #cecece;
-//   background: #fafafa;
-// `
-
-// const FooterContent = styled.div`
-//   width: 100%;
-//   max-width: 1100px;
-//   padding: 50px 10px 40px 10px;
-//   margin: 0 auto;
-//   color: #484848;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   align-items: center;
-
-//   h3 {
-//     font-size: 17px;
-//     font-weight: 900;
-//     margin-bottom: 30px !important;
-//     margin-top: 0;
-//   }
-
-//   h4 {
-//     font-size: 17px;
-//     font-weight: 500;
-//     margin-bottom: 30px;
-//   }
-
-//   li {
-//     margin-bottom: 30px;
-//   }
-
-//   li a {
-//     color: #494949;
-//     text-decoration: none;
-
-//     &:hover {
-//       color: #7a7a7a !important;
-//     }
-//   }
-
-//   .copyright {
-//     margin-top: 56px;
-//   }
-
-//   @media screen and (max-width: 420px) {
-//     flex-direction: column;
-//     text-align: center;
-//     padding: 30px 0 20px 0;
-//   }
-// `;
-
-// const FooterColLg = styled.div`
-//   width: 430px;
-//   height: 100%;
-
-//   @media screen and (max-width: 420px) {
-//     width: 100%;
-//     max-width: 300px;
-//     margin: 0 auto;
-//   }
-// `;
-
-// const FooterCol = styled.div`
-//   height: 100%;
-// `;
-
-
 import React from 'react'
 import {
   FaFacebook,
@@ -192,30 +55,29 @@ export default function Footer() {
           <FooterLinksWrapper>
             <FooterLinkItems>
               <FooterLinkTitle>
+                Navigation
+              </FooterLinkTitle>
+              <FooterLink to="/buyers">Buyers</FooterLink>
+              <FooterLink to="/vendors">Vendors</FooterLink>
+              <FooterLink to="/pricing">Pricing</FooterLink>
+              <FooterLink to="/support">Support</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>
+                Legal
+              </FooterLinkTitle>
+              <FooterLink to="/">Terms</FooterLink>
+              <FooterLink to="/">Privacy</FooterLink>
+              <FooterLink to="/">Cookies</FooterLink>
+              <FooterLink to="/">Copyright</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>
                 Contact Us
               </FooterLinkTitle>
-              <FooterLink to="/">Contact</FooterLink>
-              <FooterLink to="/">Support</FooterLink>
-              <FooterLink to="/">Destinations</FooterLink>
-              <FooterLink to="/">Sponsorships</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>
-                Videos
-              </FooterLinkTitle>
-              <FooterLink to="/">Submit Video</FooterLink>
-              <FooterLink to="/">Ambassadors</FooterLink>
-              <FooterLink to="/">Agency</FooterLink>
-              <FooterLink to="/">Influencer</FooterLink>
-            </FooterLinkItems>
-            <FooterLinkItems>
-              <FooterLinkTitle>
-                Social Media
-              </FooterLinkTitle>
-              <FooterLinkBlank to="/">Instagram</FooterLinkBlank>
-              <FooterLinkBlank to="/">Facebook</FooterLinkBlank>
-              <FooterLinkBlank to="/">Youtube</FooterLinkBlank>
-              <FooterLinkBlank to="/">Twitter</FooterLinkBlank>
+              <FooterLink to="/">24/7 chat support</FooterLink>
+              <FooterLink to="/">contact@waggon.ca</FooterLink>
+              <FooterLink to="/">+1 800-555-5555</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
